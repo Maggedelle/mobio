a3265ceaed9f49eab53f0cce8e96a9c272932de7
Maggedelle <33657258+Maggedelle@users.noreply.github.com>
2022-06-17 22:11:11 +0200

    Added player component

--- mobio-frontend/src/App.vue
+++ mobio-frontend/src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <MyConnector/>
   <MyHeader/>
-  <button v-on:click="sendMessage('hello')">Send Message</button>
-
+  <MyPlayer name="haha" x="22" y="32"/>
 </template>
 
 <script>
 import MyHeader from './components/MyHeader.vue'
 import MyConnector from './components/MyConnector.vue'
- 
+import MyPlayer from './components/MyPlayer.vue'
 
 export default {
   name: 'App',
   components: {
     MyHeader,
     MyConnector,
+    MyPlayer,
   }
 }
 </script>
---
+++ mobio-frontend/src/components/MyPlayer.vue
@@ -0,0 +1,36 @@
+<template>
+
+<div class="player" :style="cssVars">
+
+</div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'MyPlayer',
+  props: ['name','x','y'],
+   computed: {
+      cssVars () {
+        return{
+          /* variables you want to pass to css */
+          '--x': this.x + "px",
+          '--y': this.y + "px",
+        }
+    }
+   }
+}
+
+</script>
+
+<style scoped>
+.player {
+  left: var(--x);
+  bottom: var(--y);
+  position: absolute;
+  width:50px;
+  height:50px;
+  background-color:red;
+}
+</style>
