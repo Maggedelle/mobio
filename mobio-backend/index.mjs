b241efbf074f2e415b77e65b97a87a7a5b7e70d6
Maggedelle <33657258+Maggedelle@users.noreply.github.com>
2022-06-17 21:40:30 +0200

    init

---
+++ mobio-backend/index.mjs
@@ -0,0 +1,32 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import express from 'express'
+import expressWs from 'express-ws'
+import http from 'http'
+
+let port = 3000;
+
+let app = express();
+let server = http.createServer(app).listen(port);    
+
+expressWs(app, server);
+
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', (req, res) => {
+    res.status(200).send("Welcome to our app");
+});
+
+app.ws('/ws', async function(ws, req) {
+    console.log("I got a conneciton")
+    ws.on('message', async function(msg) {
+        console.log(msg);
+        ws.send(JSON.stringify({ "dillerdaller" : "hello" }));
+    });
+});
+
+console.log(`Server started on port ${port}! Enjoy :-)`)
---
+++ mobio-frontend/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+
+  <MyHeader/>
+  <button v-on:click="sendMessage('hello')">Send Message</button>
+
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+
+ 
+
+export default {
+  name: 'App',
+  components: {
+    MyHeader,
+  },
+  data() {
+        return {
+            socket: {},
+            connectedStatus: 'Not connected!',
+            message: 'No message yet!'
+        }
+    },
+    async mounted() {
+
+        const echoSocketUrl ='ws://127.0.0.1:3000/ws'
+    
+        this.socket = await new WebSocket(echoSocketUrl); 
+
+        this.socket.onopen = () => {
+            console.log('Websocket connected.');
+            this.connectedStatus = 'Connected';
+            //this.sendMessage(JSON.stringify({"message" : "Hello, server."}));
+        }
+
+        this.socket.onmessage = (event) => {
+            let parsedMessage = JSON.parse(event.data);
+            console.log(parsedMessage);
+            if(typeof parsedMessage.message !== "undefined" && parsedMessage.message == "hello") {
+                this.message = parsedMessage.message;
+                console.log('We have received a message from the server!')
+            }
+        }
+    },
+    methods: {
+        waitForOpenConnection: function() {
+            // We use this to measure how many times we have tried to connect to the websocket server
+            // If it fails, it throws an error.
+            return new Promise((resolve, reject) => {
+                const maxNumberOfAttempts = 10
+                const intervalTime = 200 
+
+                let currentAttempt = 0
+                const interval = setInterval(() => {
+                    if (currentAttempt > maxNumberOfAttempts - 1) {
+                        clearInterval(interval)
+                        reject(new Error('Maximum number of attempts exceeded.'));
+                    } else if (this.socket.readyState === this.socket.OPEN) {
+                        clearInterval(interval)
+                        resolve()
+                    }
+                    currentAttempt++
+                }, intervalTime)
+            })
+        },
+        sendMessage: async function(message) {
+            if (this.socket.readyState !== this.socket.OPEN) {
+                try {
+                    await this.waitForOpenConnection(this.socket)
+                    this.socket.send(message)
+                } catch (err) { console.error(err) }
+            } else {
+                this.socket.send(message)
+            }
+        }
+    }
+  
+}
+</script>
